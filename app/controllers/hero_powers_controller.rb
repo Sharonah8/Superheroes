d0d563f465a677599228f550797b58349bc24937
Sharonah8 <nyamongosharon88@gmail.com>
2022-12-01 20:40:39 +0300

    Add relationship between heroes and powers

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,17 @@
 class HeroPowersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_hero_power_not_created_response
+    def create
+        hero_power = HeroPower.create(hero_power_params)
+        render json: hero_power
+    end
+
+    private
+
+    def hero_power_params
+        params.permit(:strength, :power_id, :hero_id)
+    end
+
+    def render_hero_power_not_created_response
+        render json: { errors: invalid.record.errors}, status: 422
+    end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,7 @@
 class PowersController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :render_power_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
     def index
         powers = Power.all
         render json: powers 
@@ -10,9 +12,23 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_power_not_found_response
         render json: power
     end
 
+    def update
+        power = Power.find(params[:id])
+        power.update(power_params)
+        render json: power
+    end
+
     private
 
     def render_power_not_found_response
         render json: { error: "Power not found"}, status: 404
+    end
+
+    def power_params
+        params.permit(:description)
+    end
+
+    def render_invalid_response
+        render json: { errors: invalid.record.errors }, status: 422
     end
 end
