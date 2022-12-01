0974c756c9111d380ee5093f9e8e3672f9ff5af1
Sharonah8 <nyamongosharon88@gmail.com>
2022-12-01 19:51:17 +0300

    Show method for power model

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -4,4 +4,9 @@ class PowersController < ApplicationController
         powers = Power.all
         render json: powers 
     end
+
+    def show
+        power = Power.find(params[:id])
+        render json: power
+    end
 end
