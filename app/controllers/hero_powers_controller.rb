253dde9d587102b99b1882862cdfa8fd9e9ee84f
Sharonah8 <nyamongosharon88@gmail.com>
2022-12-06 20:29:16 +0300

    Validation errors

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,10 +1,11 @@
 class HeroPowersController < ApplicationController
-rescue_from ActiveRecord::RecordNotFound, with: :render_hero_power_not_created_response
-wrap_parameters format: []
+#rescue_from ActiveRecord::RecordNotFound, with: :render_hero_power_not_created_response
+rescue_from ActiveRecord::RecordInvalid, with: :record_not_found
+
     
     def create
-        hero_power = HeroPower.create(hero_power_params)
-        render json: hero_power.hero
+        hero_power = HeroPower.create!(hero_power_params)
+        render json: hero_power, serializer: HeroPowerSerializer
     end
 
     private
@@ -13,7 +14,11 @@ wrap_parameters format: []
         params.permit(:strength, :power_id, :hero_id)
     end
 
-    def render_hero_power_not_created_response
-        render json: { errors: invalid.record.errors}, status: 422
+    # def render_hero_power_not_created_response
+    #     render json: { errors: invalid.record.errors}, status: 422
+    # end
+
+    def record_not_found
+        render json: { "errors": ["validation errors"]}
     end
 end
--- app/serializers/individual_hero_serializer.rb
+++
@@ -1,3 +0,0 @@
-class IndividualHeroSerializer < ActiveModel::Serializer
-  attributes :id, :name, :super_name, :powers
-end
